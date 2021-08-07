52232169aa35251ace2d497c5c8f097a7f3e1fff
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-08-08 00:35:08 +0300

    create an error hander for 404 pages

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from config import config_options
-
-def createApp(config_name):
+#creating app configuration
+def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_options[config_name])
+    #registering the blueprint
+    from .main import main as blue_print
+    app.register_blueprint(blue_print)
 
     return app
---
+++ app/main/__init__.py
@@ -0,0 +1,4 @@
+from flask import Blueprint
+from main import error, views
+
+main = Blueprint('main', __name__)
