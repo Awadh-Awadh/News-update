ea2d0dcd9e14a4d2f0e256bce7a5e486c7b2ed58
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-08-08 18:24:55 +0300

    create an articles class

--- app/models.py
+++ app/models.py
@@ -2,3 +2,11 @@ class Source:
     def __init__(self,id,name):
       self.id = id
       self.name = name
+
+class Articles:
+    def __init__(self, publishedAt, urlToImage,title,content,author):
+      self.publishedAt = publishedAt
+      self.urlToImage= urlToImage
+      self.title = title
+      self.content =content
+      self.author = author
