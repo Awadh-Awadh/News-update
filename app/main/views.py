a015f14ea9bc593771bd678a0d05450f54e036e5
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-08-08 11:37:51 +0300

    add index template

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,8 @@
 from flask import render_template, request, url_for, redirect
 from . import main
-# from ..requests import get_news
+# # from ..requests import get_news
+# from ..models import Source
+
 
 @main.route('/')
 def index():
