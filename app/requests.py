b1a0746213c3634963fc81b85dd2cec51bf52df0
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-08-08 16:04:52 +0300

    add a method that returns a list of sources

--- app/requests.py
+++ app/requests.py
@@ -3,8 +3,17 @@ import requests as rq
 
 api_key = None
 base_url = None
+article_url = None
 
 def configure_request(app):
-    global api_key,base_url
+    global api_key,base_url,article_url
     api_key = app.config['NEWS_API_KEY']
     base_url = app.config['NEWS_API_BASE_URL']
+    article_url = app.config['NEWS_ARTICLE_URL']
+
+def get_sources():
+    with rq.get(base_url.format(api_key)) as data:
+        data = data.json()
+        source_list = data.get('sources')
+        return source_list
+
--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
 class config:
-    NEWS_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+    NEWS_API_BASE_URL ='https://newsapi.org/v2/top-headlines/sources?apiKey={}'
+    NEWS_ARTICLE_URL = 'https://newsapi.org/v2/top-headlines?sources={}-news&apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
