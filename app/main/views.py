d1f37ea57191e62e8b3ef8866a4ff3c71d125af4
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-08-08 16:31:07 +0300

    add new changes

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,12 @@
 from flask import render_template, request, url_for, redirect
 from . import main
-# # from ..requests import get_news
+from ..requests import get_sources
 # from ..models import Source
 
 
 @main.route('/')
 def index():
     message  = "Helo world"
+    news = get_sources()
 
-    return render_template('index.html', message = message)
+    return render_template('index.html', message = news)
