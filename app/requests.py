46c377aaf8d66f3fa67240eb46282cdbc671e458
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-08-08 16:58:58 +0300

    add source instance that returns an object of sources

--- app/requests.py
+++ app/requests.py
@@ -1,5 +1,5 @@
 import requests as rq
-
+from models import Source
 
 api_key = None
 base_url = None
@@ -15,5 +15,11 @@ def get_sources(category):
     with rq.get(base_url.format(category,api_key)) as data:
         data = data.json()
         source_list = data.get('sources')
-        return source_list
+        sources_object = None
+        if source_list:
+            id = source_list.get('id')
+            name = source_list.get('name')
+            sources_object = Source(id, name)
+
+        return sources_object
 
